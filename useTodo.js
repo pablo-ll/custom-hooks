11beb479097422aae686ef5b262d5cda6b75941c
pablo-ll <pablollanos823@gmail.com>
2024-05-17 19:00:55 -0400

    mis primeros hooks

---
+++ useTodo.js
@@ -0,0 +1,63 @@
+import { useEffect, useReducer } from "react";
+import { todoReducer } from "../08-useReducer/todoReducer";
+
+const initialState = [
+    /*  {
+       id: new Date().getTime(),
+       todo: 'aprender react',
+       done: false
+       },
+
+        */
+];
+
+const init = () => {
+   return JSON.parse(localStorage.getItem('todos')) || [];
+}
+
+export const useTodo = () => {
+
+    const [todos, dispatchTodo] = useReducer(todoReducer, initialState, init); 
+
+    useEffect(() => {
+        localStorage.setItem('todos', JSON.stringify(todos));
+     
+    }, [todos])
+    
+    const handloAddTodo = (todo) => {
+
+
+        dispatchTodo({
+            type: '[TODO] Add Todo',
+            payload: todo
+        })
+
+        
+    }
+
+    const  handloDeleteTodo = (id) => {
+        console.log({ id });
+        dispatchTodo({
+            type: '[TODO] Remove Todo',
+            payload: id
+        })
+    }
+
+    const handloToggleTodo = (id) => {
+        dispatchTodo({
+            type: '[TODO] Toggle Todo',
+            payload: id
+        })
+    }
+
+  return {
+
+    todos,
+    todosCount: todos.length,
+    pendingTodosCount: todos.filter( todo => !todo.done ).length,
+    handloToggleTodo,
+    handloDeleteTodo,
+    handloAddTodo
+
+  }
+}
